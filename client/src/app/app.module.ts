c451957bc18826b9a52c7042160933974c303605
Jason1811 <zhenjieh@umich.edu>
2018-01-01 20:50:00 -0500

    routes added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import { DataService } from './services/data.service';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+import { routing } from './app.routes'; 
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemListComponent,
+    ProblemDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/components/problem-list/problem-list.component.ts
+++ client/src/app/components/problem-list/problem-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Problem } from "../../models/problem.model";
-import { PROBLEMS} from "../../mock-problems";
+import { DataService } from "../../services/data.service";
 @Component({
   selector: 'app-problem-list',
   templateUrl: './problem-list.component.html',
@@ -9,13 +9,13 @@ import { PROBLEMS} from "../../mock-problems";
 export class ProblemListComponent implements OnInit {
   problems:Problem[];
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   	this.getProblems();
   }
 
   getProblems():void{
-  	this.problems = PROBLEMS;
+  	this.problems =  this.dataService.getProblems();
   }
 }
